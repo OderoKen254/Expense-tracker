d62f31d053f0bef502cf37fbe368b485d07e59f6
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-04-13 21:25:35 +0300

    add function SearchBar to handle search of expenses

--- src/Componnets/SearchBar.js
+++ src/Componnets/SearchBar.js
@@ -1,1 +1,14 @@
 
+function SearchBar({ searchTerm, setSearchTerm }) {
+    return (
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search expenses..."
+        className="search-bar"
+      />
+    );
+  }
+  
+  export default SearchBar;
